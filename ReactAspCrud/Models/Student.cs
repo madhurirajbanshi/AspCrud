7b811d704619df598adbe2b54d7f18665932b047
Madhuri <madhurirajbanshi318@gmail.com>
2024-09-10 02:40:45 -0700

    Add project files.

---
+++ ReactAspCrud/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ReactAspCrud.Models
+{
+	public class Student
+	{
+
+		public int Id { get; set; }
+		public string  stName { get; set; }
+		public string Course  { get; set;}
+
+	}
+}
---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ReactAspCrud.Models
+{
+	public class StudentDbContext : DbContext
+	{
+		public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Student> Student { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+
+		}
+	
+	}
+}
